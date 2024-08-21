0c16a4127faaea42e1dbe2a6d0e6701f8c429b62
James Daniel Cadavona <63523469+Jameboyyy@users.noreply.github.com>
2024-08-21 00:13:41 -0700

    Set Up React/Node/PostGreSql/Redux

---
+++ guiding-sheep/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { BrowserRoutes as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ guiding-sheep/src/store.js
@@ -0,0 +1,30 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+    user: null,
+};
+
+const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    reducers: {
+        login: (state, action) => {
+            state.user = action.payload;
+        },
+        logout: (state) => {
+            state.user = null;
+        },
+    },
+});
+
+export const { login, logout } = authSlice.actions;
+
+const store = configureStore({
+    reducer: {
+        auth: authSlice.reducer,
+    },
+});
+
+export default store;
