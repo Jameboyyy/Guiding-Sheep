f8e724fb854bb23b90871ff9bb245904f9db90b5
James Daniel Cadavona <63523469+Jameboyyy@users.noreply.github.com>
2024-08-23 18:19:38 -0700

    set up postgreSQL

---
+++ guiding-sheep-backend/sql/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50),
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(100),
+    create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
