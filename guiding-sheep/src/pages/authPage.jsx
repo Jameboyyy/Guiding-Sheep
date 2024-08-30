d4526c24f6fd000aec80d1927b1afa61d98ae041
James Daniel Cadavona <63523469+Jameboyyy@users.noreply.github.com>
2024-08-30 01:56:58 -0700

    Changed local posgresql to aws posgres

---
+++ guiding-sheep/src/pages/authPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Auth from '../components/auth';
+
+const AuthPage = () => {
+    return (
+        <section id='authPage'>
+            <Auth />
+        </section>
+    );
+}
+
+export default AuthPage;
